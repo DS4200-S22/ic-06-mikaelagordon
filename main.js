aad35bf6f670752285324e9adc1c2d39626cd9d5
Mikaela Gordon <mikaelagordonn@gmail.com>
2022-02-10 13:17:26 -0500

    what got done by end of class

--- main.js
+++ main.js
@@ -9,6 +9,18 @@ Modified: 12/06/21
 //Starter code for Button 1 click
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
+
 function button1Clicked() {
+
+    let number = Math.random()*100;
+
+    let buttonDiv = document.getElementByID("button-div");
+    buttonDiv.innerHTML = number;
     console.log("Button 1 was clicked"); 
+}
+
+function button2Clicked() {
+
+    let buttonCircle= document.getElementByID("random_num");
+     
 }
